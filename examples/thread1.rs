5847c0f8df179a107ca20c7b9c07c7740ff55173
work <work@debian-pc.debian-pc.com>
2024-04-25 03:15:01 -0400

    feature: first try on rust thread

---
+++ examples/thread1.rs
@@ -0,0 +1,54 @@
+use anyhow::Result;
+use std::{sync::mpsc, thread};
+
+const NUM_PRODUCERS: usize = 4;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Msg {
+    idx: usize,
+    value: usize,
+}
+
+fn main() -> Result<()> {
+    let (tx, rx) = mpsc::channel();
+
+    for idx in 0..NUM_PRODUCERS {
+        let tx = tx.clone();
+        thread::spawn(move || producer(idx, tx));
+    }
+    drop(tx);
+
+    let consumer = thread::spawn(move || {
+        for msg in rx {
+            println!("consumer: {:?}", msg);
+        }
+    });
+
+    consumer.join().map_err(|e| anyhow::anyhow!("{:?}", e))?;
+
+    Ok(())
+}
+
+fn producer(idx: usize, tx: mpsc::Sender<Msg>) -> Result<()> {
+    loop {
+        let value = rand::random::<usize>();
+        tx.send(Msg::new(idx, value))?;
+
+        let sleep_time = rand::random::<u8>() as u64 * 10;
+        thread::sleep(std::time::Duration::from_millis(sleep_time));
+
+        if rand::random::<u8>() % 10 == 0 {
+            println!("producer {} exit", idx);
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+impl Msg {
+    fn new(idx: usize, value: usize) -> Self {
+        Self { idx, value }
+    }
+}
